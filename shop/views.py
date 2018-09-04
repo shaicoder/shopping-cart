cf75c5fa06e7be374ac5c04765ef44f480431df5
Amit Singh Pal <30419215+amit9651@users.noreply.github.com>
2018-09-05 00:48:07 +0530

    addded templates (#1)
    * addded templates
    
    * Update list.html

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render or get_object_or_404
+from django.shortcuts import render,get_object_or_404
 from .models import Category,Product
 
 def product_list(request, category_slug=None):
